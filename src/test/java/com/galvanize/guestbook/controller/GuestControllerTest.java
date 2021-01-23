34a42782a6dcb7309432190dd4afc74ebff12061
jenndalt <jennifer.dalton@cognizant.com>
2021-01-23 11:51:28 -0600

    updated mock mvc test

--- src/test/java/com/galvanize/guestbook/controller/GuestControllerTest.java
+++ src/test/java/com/galvanize/guestbook/controller/GuestControllerTest.java
@@ -1,26 +1,29 @@
 package com.galvanize.guestbook.controller;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@AutoConfigureWebMvc
+@AutoConfigureMockMvc
 class GuestControllerTest {
 
     @Autowired
     MockMvc mockMvc;
 
     @Test
-    public void addCommentByUserTest() throws Exception {
-        mockMvc.perform(post("/guestbook"))
+    void addCommentByUserTest() throws Exception {
+        mockMvc.perform(
+                post("/guestbook"))
                 .andExpect(status().isCreated());
     }
-
 }
